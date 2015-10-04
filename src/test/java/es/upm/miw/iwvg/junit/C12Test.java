cbb3de36d32d1398bcb301f4ca9b6e66e2dff059
AdrianSantosSDG <adrian.santos@sdggroup.com>
2015-10-04 03:07:02 +0200

    C12Test

---
+++ src/test/java/es/upm/miw/iwvg/junit/C12Test.java
@@ -0,0 +1,14 @@
+package es.upm.miw.iwvg.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class C12Test {
+
+	@Test
+	public void mATest(){
+		assertEquals("mA",new C12().mA());
+	}
+	
+}
